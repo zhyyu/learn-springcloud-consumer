5097a5a7b461998df376c2e44fc95892ef23ecd6
juror.yu <juror.yu@mart.blue>
2019-07-22 09:39:27 +0800

    hystrix test

--- src/main/java/com/zhyyu/learn/springcloud/consumer/LearnSpringcloudConsumerApplication.java
+++ src/main/java/com/zhyyu/learn/springcloud/consumer/LearnSpringcloudConsumerApplication.java
@@ -2,9 +2,11 @@ package com.zhyyu.learn.springcloud.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 @SpringBootApplication
 public class LearnSpringcloudConsumerApplication {
 
--- src/main/java/com/zhyyu/learn/springcloud/consumer/controller/ConsumerController.java
+++ src/main/java/com/zhyyu/learn/springcloud/consumer/controller/ConsumerController.java
@@ -1,5 +1,6 @@
 package com.zhyyu.learn.springcloud.consumer.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,9 +22,42 @@ public class ConsumerController {
         return "hello";
     }
 
+    /**
+     * provider 异常情况:
+     * <pre>
+     *     1. 未设置 hystrix, 先报
+     *     {
+     *     "timestamp": "2019-07-19T08:25:14.046+0000",
+     *     "status": 500,
+     *     "error": "Internal Server Error",
+     *     "message": "I/O error on GET request for \"http://cloud-provider1/provider/hello\": Connection refused: connect; nested exception is java.net.ConnectException: Connection refused: connect",
+     *     "path": "/consumer/hello-from-provider"
+     *      }
+     *      异常, 后reg 剔除异常服务, 返回正常结果"hello"
+     *
+     *      2. 设置 hystrix, 先报 fallbackMethod 返回结果"provider-error", 后reg 剔除异常服务, 返回正常结果"hello"
+     * </pre>
+     * @return
+     */
     @RequestMapping("hello-from-provider")
+    @HystrixCommand(fallbackMethod = "helloFromProviderFallback")
     public String helloFromProvider() {
         return restTemplate.getForEntity("http://cloud-provider1/provider/hello", String.class).getBody();
+    }
+
+    public String helloFromProviderFallback() {
+        return "provider-error";
+    }
+
+    /**
+     * exception: No instances available for baidu.com
+     * <pre>
+     *     若 @LoadBalanced 加在 restTemplate 上, 则搜索对应cloud 服务, 无则异常
+     * </pre>
+     */
+    @RequestMapping("hello-from-other")
+    public String helloFromOther() {
+        return restTemplate.getForEntity("http://baidu.com", String.class).getBody();
     }
 
 }
